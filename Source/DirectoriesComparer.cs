b288916f4167d814090d32c465f275b19107f62f
Yehor Boldarev <yehorboldarev@seznam.cz>
2025-11-07 23:57:13 +0100

    Extended compare algorithm and added main program loop

--- Source/DirectoriesComparer.cs
+++ Source/DirectoriesComparer.cs
@@ -11,33 +11,35 @@
             ReplicaDirectoryPath = replicaDirectoryPath;
         }
 
-        public bool CheckReplicaDirectory()
-            => Directory.Exists(ReplicaDirectoryPath);
-
-        public bool CheckSourceDirectory()
-            => Directory.Exists(SourceDirectoryPath);
-
-        public (List<FileInfo>, List<FileInfo>, List<FileInfo>) Compare()
+        public ComparisonResult Compare()
         {
+            List<string> sourceDirectoryTree = Directory.GetDirectories(SourceDirectoryPath, "*", SearchOption.AllDirectories)
+                .Select(p => Path.GetRelativePath(SourceDirectoryPath, p)).ToList();
+            List<string> replicaDirectoryTree = Directory.GetDirectories(ReplicaDirectoryPath, "*", SearchOption.AllDirectories)
+                .Select(p => Path.GetRelativePath(ReplicaDirectoryPath, p)).ToList();
+
+            List<string> directoriesToAdd = sourceDirectoryTree.Except(replicaDirectoryTree).ToList();
+            List<string> directoriesToRemove = replicaDirectoryTree.Except(sourceDirectoryTree).ToList();
+
             string[] sourceDirectoryFilePaths = Directory.GetFiles(SourceDirectoryPath, "*", SearchOption.AllDirectories);
             string[] replicaDirectoryFilePaths = Directory.GetFiles(ReplicaDirectoryPath, "*", SearchOption.AllDirectories);
 
             var sourceFilesDictionary = GetFilesDictionary(sourceDirectoryFilePaths, SourceDirectoryPath);
             var replicaFilesDictionary = GetFilesDictionary(replicaDirectoryFilePaths, ReplicaDirectoryPath);
 
-            List<FileInfo> filesToAdd = new();
-            List<FileInfo> filesToUpdate = new();
-            List<FileInfo> filesToRemove = new();
+            Dictionary<string, FileInfo> filesToAdd = new();
+            Dictionary<string, FileInfo> filesToUpdate = new();
+            Dictionary<string, FileInfo> filesToRemove = new();
 
             foreach (KeyValuePair<string, FileInfo> sourceKvp in sourceFilesDictionary)
             {
                 if (!replicaFilesDictionary.TryGetValue(sourceKvp.Key, out var replicaValue))
                 {
-                    filesToAdd.Add(sourceKvp.Value);
+                    filesToAdd.Add(sourceKvp.Key, sourceKvp.Value);
                 }
                 else if (!FileComparer.Instance.Equals(sourceKvp.Value, replicaValue))
                 {
-                    filesToUpdate.Add(sourceKvp.Value);
+                    filesToUpdate.Add(sourceKvp.Key, sourceKvp.Value);
                 }
             }
 
@@ -45,11 +47,11 @@
             {
                 if (!sourceFilesDictionary.ContainsKey(replicaKvp.Key))
                 {
-                    filesToRemove.Add(replicaKvp.Value);
+                    filesToRemove.Add(replicaKvp.Key, replicaKvp.Value);
                 }
             }
 
-            return (filesToAdd, filesToUpdate, filesToRemove);
+            return new ComparisonResult(filesToAdd, filesToUpdate, filesToRemove, directoriesToAdd, directoriesToRemove);
         }
 
         private Dictionary<string, FileInfo> GetFilesDictionary(string[] filePaths, string baseDirectory)
@@ -67,4 +69,12 @@
             return result;
         }
     }
+
+    public record ComparisonResult(
+        Dictionary<string, FileInfo> FilesToAdd,
+        Dictionary<string, FileInfo> FilesToUpdate,
+        Dictionary<string, FileInfo> FilesToRemove,
+        List<string> DirectoriesToAdd,
+        List<string> DirectoriesToRemove
+    );
 }
